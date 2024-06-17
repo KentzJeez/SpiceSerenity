5c145ebfca9148dfcd8b33b274a3442735238b3e
Sasindu <sasindusavi@gmail.com>
2024-06-17 22:29:02 +0530

    Crud web application
    Crud web app using php,Js,mysql

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick =() =>
+{
+	searchForm.classList.toggle('active');
+	 loginForm.classList.remove('active');
+	navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+
+ window.onscroll=() =>
+{
+	searchForm.classList.remove('active');
+	 loginForm.classList.remove('active');
+	navbar.classList.remove('active');
+	 
+} 
+
+ 
+ var swiper = new Swiper(".product-slider", {
+     loop:true,
+      spaceBetween: 20,
+	 autoplay:{
+		 delay:7500,
+		 disableOnInteraction: false,
+	 },
+     
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        
+        },
+        768: {
+          slidesPerView: 2,
+          
+        },
+        1020: {
+          slidesPerView: 3,
+          
+        },
+      },
+    });
